591148e805419299e34707f76614a95561c56bd2
koki2402 <kartikdhingra666666@gmail.com>
2026-06-04 19:48:36 +0530

    Feature: Added Basic Express Server Setup with dotenv and nodemon

---
+++ src/config/server.config.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+    PORT: process.env.PORT || 3000 //if at any point of time someone forgets dotenv || we used this. This is though not the way, you can creat eone feature branch of this
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const {PORT} = require('./config/server.config')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended: true})); //qs library is used when you do extended:true
+app.use(bodyParser.text());
+
+app.get('/ping', (req, res) => {
+    return res.json({message: 'Problem Service is Alive'});
+})
+
+app.listen(PORT, () => {
+console.log('Server Started at port: ${PORT}');
+});
+//installing nodemon, make sure you add under scripts in package.json
+//"dev": "npx nodemon src/index.js",
+//npm run dev, is only required to run at port 3000
