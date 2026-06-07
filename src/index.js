47c4075cb5228901d9fd2f88d4238a6e245dcb82
koki2402 <kartikdhingra666666@gmail.com>
2026-06-07 17:43:07 +0530

    Feature: Added mongoose db config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {PORT} = require('./config/server.config');
 const apiRouter = require('./routes');
 const BaseError = require('./errors/baseError');
 const ErrorHandler = require('./utils/errorHandler');
+const connectToDb = require('./config/db.config');
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.use('/api', apiRouter);
 
 app.use(ErrorHandler);  //Let's have it after all the requests.
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
 console.log(`Server Started at port: ${PORT}`);
+await connectToDb();
+console.log("Successfully Connected to DB");
 });
